1201bfee0dc3e6e647c81a0fba86c004bcd7bde8
Gabriel Cyganek <gabrielcyganek@gmail.com>
2022-03-19 13:59:43 +0100

    form submit endpoint working

--- rest/src/main/java/gcyganek/rest/controller/FormSubmitController.java
+++ rest/src/main/java/gcyganek/rest/controller/FormSubmitController.java
@@ -2,18 +2,20 @@ package gcyganek.rest.controller;
 
 import gcyganek.rest.apicaller.OmdbApiCaller;
 import gcyganek.rest.apicaller.TmdbApiCaller;
-import gcyganek.rest.model.FormWithImdbId;
-import gcyganek.rest.model.OmdbMovieRatings;
-import gcyganek.rest.model.TmdbMovieRatings;
+import gcyganek.rest.model.*;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 public class FormSubmitController {
 
-    @PostMapping("/form-submit")
-    public void formSubmittedHandling(@ModelAttribute FormWithImdbId form) {
+    @PostMapping(value = "/form-submit", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView formSubmittedHandling(@ModelAttribute FormWithImdbId form) {
 
         OmdbMovieRatings omdbMovieRatings = new OmdbMovieRatings();
         TmdbMovieRatings tmdbMovieRatings = new TmdbMovieRatings();
@@ -32,12 +34,59 @@ public class FormSubmitController {
             e.printStackTrace();
         }
 
-        System.out.println(omdbMovieRatings.getAverageRating());
-        omdbMovieRatings.getMovieRatings().forEach(a -> System.out.print(a + " "));
-        System.out.println(omdbMovieRatings.getEndStatus());
-        System.out.println();
-        System.out.println(tmdbMovieRatings.getAverageRating());
-        System.out.println(tmdbMovieRatings.getEndStatus());
+        return createAndGetModelAndView(List.of(tmdbMovieRatings, omdbMovieRatings), omdbMovieRatings.getMovieName());
+    }
+
+    private ModelAndView createAndGetModelAndView(List<MovieRatings> movieRatingsList, String movieTitle) {
+        ModelAndView mav = new ModelAndView("movie-ratings");
+
+        movieRatingsList.forEach(movieRatings -> processMovieRatings(mav, movieRatings));
+
+        mav.addObject("movieTitle", movieTitle);
+        mav.addObject("averageMovieRating", getAverageMovieRatingFromMovieRatingsList(movieRatingsList));
+
+        return mav;
+    }
+
+    private double getAverageMovieRatingFromMovieRatingsList(List<MovieRatings> movieRatingsList) {
+        List<MovieRatings> sourcesToConsiderList = movieRatingsList.stream()
+                .filter(movieRatings -> movieRatings.getEndStatus() == ApiCallerEndStatus.OK
+                            && movieRatings.getMovieRatings().size() != 0)
+                .toList();
+
+        if (sourcesToConsiderList.size() == 0)
+            return -1;
+
+        return sourcesToConsiderList.stream()
+                .map(MovieRatings::getAverageRating)
+                .reduce(0.0, Double::sum) / sourcesToConsiderList.size();
+    }
+
+    private void processMovieRatings(ModelAndView mav, MovieRatings movieRatings) {
+        ApiCallerEndStatus endStatus = movieRatings.getEndStatus();
+
+        if (endStatus == ApiCallerEndStatus.OK) {
+            String attributeName = getMovieRatingsAttributeName(movieRatings);
+            mav.addObject(attributeName, movieRatings);
+        }
+        else {
+            String attributeName = getErrorAttributeName(movieRatings);
+            mav.addObject(attributeName, endStatus);
+        }
+    }
+
+    private String getMovieRatingsAttributeName(MovieRatings movieRatings) {
+        return switch (movieRatings.getApiName()) {
+            case OMDb_API -> "omdbMovieRatings";
+            case TMDB_API -> "tmdbMovieRatings";
+        };
+    }
+
+    private String getErrorAttributeName(MovieRatings movieRatings) {
+        return switch (movieRatings.getApiName()) {
+            case OMDb_API -> "omdbMovieRatingsError";
+            case TMDB_API -> "tmdbMovieRatingsError";
+        };
     }
 
 }
